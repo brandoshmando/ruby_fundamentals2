43471f46760513d645288f09b3201929ad6ee36e
brandoshmando <brancraft@gmail.com>
2014-06-03 15:37:48 -0400

    Put print list into a method and added rice to the list and called method to output a second time

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,11 @@
-list = ["bananas", "milk", "jelpenos", "chocolate", "more chocolate"]
-
-list.each do |element|
-	puts "*#{element}"
+def print_list(items)
+	items.each do |element|
+		puts "*#{element}"
+	end
 end
+
+list = ["bananas", "milk", "jelpenos", "chocolate", "more chocolate"]
+print_list(list)
+
+list.push("rice")
+print_list(list)
