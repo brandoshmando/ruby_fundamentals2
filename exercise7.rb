ba4b5be97ff1b857368b5a9f72aec3ad7bdca0e5
brandoshmando <brancraft@gmail.com>
2014-06-03 17:18:05 -0400

    Final commit

--- exercise7.rb
+++ exercise7.rb
@@ -1,6 +1,6 @@
 def display_cohorts(hash)
 	hash.each do |key, value|
-		puts "#{key}: #{value*1.05}"
+		puts "#{key}: #{(value*1.05).to_i}"
 	end
 end
 
@@ -9,6 +9,9 @@ students = {
 	:cohort2 => 42,
 	:cohort3 => 22
 }
+
+puts students.values.inject(&:+)
+
 
 display_cohorts(students)
 puts
@@ -19,3 +22,4 @@ students.keys
 puts
 students.delete (:cohort2)
 display_cohorts(students)
+
