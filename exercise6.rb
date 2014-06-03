a11baea6faed923a51ccf68e2aae0030aac9e4a1
brandoshmando <brancraft@gmail.com>
2014-06-03 15:58:28 -0400

    Sorted list before output

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,6 @@
 def print_list(items)
 	puts "There are #{items.size} items on your list. Count em up!"
-	items.each do |element|
+	items.sort.each do |element|
 		puts "*#{element}"
 	end
 	if items.include?("bananas")
